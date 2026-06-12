07634d1cbb60050874e10a883b74688b4f182ac4
Jaspreet singh <jsingh15_be24@thapar.edu>
2026-06-12 16:25:16 +0530

    Refactor birthday email sending logic and file paths

--- main.py
+++ main.py
@@ -17,9 +17,7 @@ today_day = now.day
 
 
 # HINT 2: Use pandas to read the birthdays.csv
-df = pd.read_csv(
-    r"D:\study_skills\udemy_python\birthday-wisher-normal-start\birthdays.csv"
-)
+df = pd.read_csv("birthdays.csv")
 # HINT 3: Use dictionary comprehension to create a dictionary from birthday.csv that is formated like this:
 # birthdays_dict = {
 #     (birthday_month, birthday_day): data_row
@@ -43,37 +41,33 @@ today = (today_month, today_day)
 # if (today_month, today_day) in birthdays_dict:
 if today in birthdays_dict:
     birthday_person = birthdays_dict[today]
-# 3. If there is a match, pick a random letter (letter_1.txt/letter_2.txt/letter_3.txt) from letter_templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT 1: Think about the relative file path to open each letter. 
-# HINT 2: Use the random module to get a number between 1-3 to pick a randome letter.
-# HINT 3: Use the replace() method to replace [NAME] with the actual name. https://www.w3schools.com/python/ref_string_replace.asp
-import random
 
-letter_num = random.randint(1, 3)
+    letter_num = random.randint(1, 3)
 
-with open(
-    rf"D:\study_skills\udemy_python\birthday-wisher-normal-start\letter_templates\letter_{letter_num}.txt"
-) as letter_file:
-    letter_contents = letter_file.read()
-letter_contents = letter_contents.replace(
-    "[NAME]",
-    birthday_person["name"]
-)
+    with open(f"letter_templates/letter_{letter_num}.txt") as letter_file:
+        letter_contents = letter_file.read()
+
+    letter_contents = letter_contents.replace(
+        "[NAME]",
+        birthday_person["name"]
+    )
+    my_email = os.environ.get("MY_EMAIL")
+    password = os.environ.get("MY_PASSWORD")
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(my_email, password)
+
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday\n\n{letter_contents}"
+        )
 # 4. Send the letter generated in step 3 to that person's email address.
 # HINT 1: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
 # HINT 2: Remember to call .starttls()
 # HINT 3: Remember to login to your email service with email/password. Make sure your security setting is set to allow less secure apps.
 # HINT 4: The message should have the Subject: Happy Birthday then after \n\n The Message Body.
-my_email = os.environ.get("MY_EMAIL")
-password = os.environ.get("MY_PASSWORD")
-with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-    connection.starttls()
-    connection.login(my_email, password)
-    print("sending.......")
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs=birthday_person["email"],
-        msg=f"Subject:Happy Birthday\n\n{letter_contents}"
-    )
-    print("sent")
+
+
 
